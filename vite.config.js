9af0c581478e2b8955f2ccb65ca76636a76e2fa3
robwilsoncaldosa <21103830@usc.edu.ph>
2023-12-04 03:33:27 +0800

    update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import path from 'path'
 export default defineConfig({
     plugins: [
         laravel([
+            'resources/css/app.css',
             'resources/js/app.js',
         ]),
     ],
