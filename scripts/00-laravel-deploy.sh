f87a098d262cb5b9bc0569e007acb0fd813eb097
robwilsoncaldosa <21103830@usc.edu.ph>
2023-12-04 02:57:22 +0800

    update scripts

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -3,8 +3,10 @@ echo "Running composer"
 composer global require hirak/prestissimo
 composer install --no-dev --working-dir=/var/www/html
 
-echo "generating application key..."
-php artisan key:generate --show
+# echo "generating application key..."
+# php artisan key:generate --show
+
+npm run dev
 
 echo "Caching config..."
 php artisan config:cache
