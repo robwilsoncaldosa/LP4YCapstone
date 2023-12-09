1db0da2f0e7193f23d9df1a3da7cb9ec0c91bf65
robwilsoncaldosa <21103830@usc.edu.ph>
2023-12-10 01:12:11 +0800

    fixed add rooms image to display

--- app/Http/Controllers/RoomController.php
+++ app/Http/Controllers/RoomController.php
@@ -14,7 +14,7 @@ class RoomController extends Controller
     public function showBookingView()
     {
         $rooms = Room::all();
-        return view('book',compact('rooms'));
+        return view('book', compact('rooms'));
 
     }
 
@@ -22,45 +22,61 @@ class RoomController extends Controller
      * Show the form for creating a new resource.
      */
     public function create()
-{
-    return view('dashboard.create_room');
-}
+    {
+        return view('dashboard.create_room');
+    }
 
     /**
      * Store a newly created resource in storage.
      */
     public function store(Request $request)
-{
-    // Validate the input
-    $request->validate([
-        'room_name' => 'required|unique:rooms',
-        'description' => 'required',
-        'accommodates' => 'required|numeric',
-        'beds' => 'required|numeric',
-        'bed_type' => 'required',
-        'amenities' => 'required',
-        'price_per_night' => 'required|numeric',
-        'image_path' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
-    ]);
+    {
+        // Validate the input
+        $request->validate([
+            'room_name' => 'required|unique:rooms',
+            'description' => 'required',
+            'accommodates' => 'required|numeric',
+            'beds' => 'required|numeric',
+            'bed_type' => 'required',
+            'amenities' => 'required',
+            'price_per_night' => 'required|numeric',
+            'image_path' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+        ]);
 
-    // Upload the image
-    $imagePath = $request->file('image_path')->store('room_images');
+        // Upload the image
+        $image = $request->file('image_path');
+        $originalFilename = $image->getClientOriginalName();
+        $filename = pathinfo($originalFilename, PATHINFO_FILENAME);
+        $extension = $image->getClientOriginalExtension();
+        $timestamp = now()->timestamp;
+        $uniqueFilename = "{$filename}_{$timestamp}.{$extension}";
 
-    // Create the room
-    $room = Room::create([
-        'room_name' => $request->input('room_name'),
-        'description' => $request->input('description'),
-        'accommodates' => $request->input('accommodates'),
-        'beds' => $request->input('beds'),
-        'bed_type' => $request->input('bed_type'),
-        'amenities' => $request->input('amenities'),
-        'price_per_night' => $request->input('price_per_night'),
-        'image_path' => $imagePath,
-    ]);
+        // Specify the absolute path to public/img
+        $image->move(public_path('img'), $uniqueFilename);
 
-    // Redirect or respond as needed
-    return redirect()->route('dashboard.rooms')->with('success', 'Room added successfully.');
-}
+        // Set the image path for database storage
+        $imagePath = "img/{$uniqueFilename}";
+
+        // Create the room
+        $room = Room::create([
+            'room_name' => $request->input('room_name'),
+            'description' => $request->input('description'),
+            'accommodates' => $request->input('accommodates'),
+            'beds' => $request->input('beds'),
+            'bed_type' => $request->input('bed_type'),
+            'amenities' => $request->input('amenities'),
+            'price_per_night' => $request->input('price_per_night'),
+            'image_path' => $imagePath,
+        ]);
+
+
+
+
+
+
+        // Redirect or respond as needed
+        return redirect()->route('dashboard.rooms')->with('success', 'Room added successfully.');
+    }
 
 
     /**
@@ -82,7 +98,7 @@ class RoomController extends Controller
         $room = Room::findOrFail($id);
         return view('dashboard.edit_room', compact('room'));
     }
-    
+
 
 
     /**
@@ -101,17 +117,17 @@ class RoomController extends Controller
             'price_per_night' => 'required|numeric',
             'image_path' => 'image|mimes:jpeg,png,jpg,gif,svg|max:2048',
         ]);
-    
+
         // Find the room by ID
         $room = Room::findOrFail($id);
-    
+
         // Update the room
         $room->update($request->all());
-    
+
         // Redirect or respond as needed
         return redirect()->route('dashboard.rooms')->with('success', 'Room updated successfully.');
     }
-    
+
 
     /**
      * Remove the specified resource from storage.
@@ -120,14 +136,14 @@ class RoomController extends Controller
     {
         // Find the room by ID
         $room = Room::findOrFail($id);
-    
+
         // Delete the room
         $room->delete();
-    
+
         // Redirect or respond as needed
         return redirect()->route('dashboard.rooms')->with('success', 'Room deleted successfully.');
     }
-    
+
 
 
     public function showAllRooms()
@@ -137,19 +153,19 @@ class RoomController extends Controller
         return view('dashboard', ['rooms' => $rooms]);
     }
 
-      public function showRoomStatus()
+    public function showRoomStatus()
     {
-        
+
 
         $roomStatuses = DB::table('rooms')
-        ->select('rooms.room_name', DB::raw("CASE WHEN reservations.checked_out_at >= NOW() THEN CONCAT('Occupied until ', DATE_FORMAT(reservations.checked_out_at, '%M %e, %Y at %l:%i %p')) ELSE 'Available' END as status"))
-        ->leftJoin('reservations', function ($join) {
-            $join->on('rooms.id', '=', 'reservations.room_id')
-                ->where('reservations.checked_out_at', '=', DB::raw('(SELECT MAX(checked_out_at) FROM reservations WHERE room_id = rooms.id)'));
-        })
-        ->get();
-    
-    return view('dashboard', ['roomStatuses' => $roomStatuses]);
-    
-}
+            ->select('rooms.room_name', DB::raw("CASE WHEN reservations.checked_out_at >= NOW() THEN CONCAT('Occupied until ', DATE_FORMAT(reservations.checked_out_at, '%M %e, %Y at %l:%i %p')) ELSE 'Available' END as status"))
+            ->leftJoin('reservations', function ($join) {
+                $join->on('rooms.id', '=', 'reservations.room_id')
+                    ->where('reservations.checked_out_at', '=', DB::raw('(SELECT MAX(checked_out_at) FROM reservations WHERE room_id = rooms.id)'));
+            })
+            ->get();
+
+        return view('dashboard', ['roomStatuses' => $roomStatuses]);
+
+    }
 }
