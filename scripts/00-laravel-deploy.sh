ba4b71c1f61c3417ba240948b24e43992d7cde37
robwilsoncaldosa <21103830@usc.edu.ph>
2023-12-17 21:01:07 +0800

    final attempt to fix the problem

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -13,6 +13,6 @@ echo "Caching routes..."
 php artisan route:cache
 
 echo "Running migrations..."
-php artisan migrate --force
+php artisan migrate:fresh --force
 
 php artisan db:seed --class=PersonnelSeeder --force
