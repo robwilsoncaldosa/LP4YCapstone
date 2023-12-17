7344e49b9e48544ae7e9afe7d0b85301d68d14c3
robwilsoncaldosa <21103830@usc.edu.ph>
2023-12-18 05:58:19 +0800

    register google console

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="google-site-verification" content="v34TmExAXeZR2hc0DiPYDmeqE_oEu5EEz_OxumPwN28" />
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('title', 'Default Title')</title>
